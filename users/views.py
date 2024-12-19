c4df1725856faf0dd7d8394b977c61545c1ec933
Chad Jemmett <chad.jemmett@gmail.com>
2024-12-19 15:31:57 -0600

    updating views to builtin views

--- users/views.py
+++ users/views.py
@@ -34,19 +34,6 @@ def register(request):
         return render(request, 'registration/register.html', {"form": form })
 
 
-def user_login(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            redirect("dashboard")
-            
-    else:
-        form = AdvisorLoginForm()
-        return render(request, "registration/login.html", {"form": form})
-
 
 
 
